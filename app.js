0588c8013e9f7c1d96f911593a6f37757099ec46
Yash Kamaat <yashtkamaat@gmail.com>
2021-11-08 10:33:04 -0500

    Pre-bonus

--- app.js
+++ app.js
@@ -16,9 +16,13 @@ console.log(jokesJS1);
 //=================
 //1b. Access the value for the "setup" key in the jokeJS1 object and set it to the inner text for the p1 variable/element (If done correctly the setup for the joke should display on the webpage)
 p1.innerText = jokesJS1.setup;
+//OR
+//p1.innerHTML = jokesJS1.setup;
 //====================
 //1c. Access the value for the "punchline" key in the jokeJS1 object and set it to the inner text for the p2 variable/element (If done correctly the punchline for the joke should display on the webpage)
 p2.innerText = jokesJS1.punchline;
+//OR
+//p2.innerHTML = jokesJS1.punchline;
 console.log(jokesJS1.setup);
 //======================
 // 2
@@ -28,6 +32,27 @@ const p4 = document.createElement('p');
 second.append(p3);
 second.append(p4);
 
+//2a. Use the "Returns a single random quote." endpoint/URL to GET a Random quote using axios
+axios.get(`https://friends-quotes-api.herokuapp.com/quotes/random`)
+//================
+//2b. Use .then to create a function that sets the value returned from the axios GET request to a const variable called friendsJS2 (NOTE: You will need to create a function with a parameter. The parameter will hold the "resolved" value returned from the axios GET request).
+.then(res => {
+    console.log(res);
+    const friendsJS2 = res.data;
+    console.log(res.data);
+
+//================
+//2c. Inside of the same function, access the value for the "character" key in the friendsJS2 object and set it to the inner text for the p3 variable/element. Also, access the value for the "quote" key in the friendsJS2 object and set it to the inner text for the p4 variable/element (If done correctly the character and quote for the quote should display on the webpage)
+p3.innerText = friendsJS2.character;
+p4.innerText = friendsJS2.quote;
+})
+//===================
+//2d. Finally, use .catch to create a function that would display the "rejected" value/error returned from the axios GET request in the console (NOTE: You will need a console log for this. Also, you will need to create a function with a parameter. The parameter will hold the "rejected" value/error returned from the axios GET request).
+.catch(err => {
+    console.log(`Question 2 Failed`);
+    console.log(err);
+});
+
 // 3
 const third = document.querySelector('#third');
 const p5 = document.createElement('p');
@@ -35,6 +60,22 @@ const p6 = document.createElement('p');
 third.append(p5);
 third.append(p6);
 
+
+//3b
+async function quoteFunc (){
+    try {
+      // 3a
+      const quoteJS3 = await axios.get(`https://friends-quotes-api.herokuapp.com/quotes/random`);
+      
+    //3c
+    p5.innerText = quoteJS3.data.character;
+    p6.innerText = quoteJS3.data.quote;
+    //3d
+} catch (err) {
+    console.log(err);
+  }
+}
+quoteFunc();
 // 4
 const fourth = document.querySelector('#fourth');
 const p7 = document.createElement('p');
